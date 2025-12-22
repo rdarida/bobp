f09d6c625134a6bc0f2f87ab49928fff903898fd
Róbert Darida <rdarida@users.noreply.github.com>
2025-12-22 13:08:29 +0100

    feat: implement updatePackageJson for next command

--- src/next.ts
+++ src/next.ts
@@ -1,12 +1,35 @@
 import { resolve } from 'node:path';
 
 import degit from 'degit';
+import { existsSync, readFileSync, writeFileSync } from 'node:fs';
 
 export type NextOptions = {
   name: string;
 };
 
-export async function next({ name }: NextOptions): Promise<void> {
+export async function next(nextOptions: NextOptions): Promise<void> {
+  await cloneNextTemplate(nextOptions);
+  updatePackageJson(nextOptions);
+}
+
+async function cloneNextTemplate({ name }: NextOptions): Promise<void> {
   const emitter = degit('rdarida/template-next#main');
   return await emitter.clone(resolve(process.cwd(), name));
+}
+
+function updatePackageJson({ name }: NextOptions): void {
+  const packageJsonPath = resolve(process.cwd(), name, 'package.json');
+
+  if (!existsSync(packageJsonPath)) {
+    return;
+  }
+
+  const content = readFileSync(packageJsonPath, { encoding: 'utf-8' });
+
+  const object = {
+    ...JSON.parse(content),
+    name
+  };
+
+  writeFileSync(packageJsonPath, JSON.stringify(object, null, 2));
 }
