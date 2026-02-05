1b3fadf07149217ca44540324dc135dfb19b4224
Róbert Darida <rdarida@users.noreply.github.com>
2026-02-06 00:15:57 +0100

    test: use temp directory as path of the electron command

--- tests/electron.test.ts
+++ tests/electron.test.ts
@@ -1,23 +1,19 @@
 import { existsSync } from 'node:fs';
 import { join } from 'node:path';
 
-import { rimrafSync } from 'rimraf';
-
 import { normalize } from '../src/utils';
 import { electron } from '../src/electron';
+
+import { TEST_TEMP_DIR } from './constants';
 
 describe('Test electron function', () => {
   const productName = 'Electron App';
   const name = normalize(productName);
-  const outputPath = join(process.cwd(), name);
+  const outputPath = join(TEST_TEMP_DIR, name);
 
   it('should create an Electron project', async () => {
-    await electron({ name, productName, path: process.cwd() });
+    await electron({ name, productName, path: TEST_TEMP_DIR });
 
     expect(existsSync(outputPath)).toBe(true);
-  });
-
-  afterEach(() => {
-    rimrafSync(outputPath);
   });
 });
