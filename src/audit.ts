d0fdf87b1d354dc5d35ed68033591ce18c52ccae
Róbert Darida <rdarida@users.noreply.github.com>
2026-09-23 01:10:43 +0200

    feat: implement audit feature

--- src/audit.ts
+++ src/audit.ts
@@ -1,4 +1,8 @@
 import { existsSync, readFileSync } from 'node:fs';
+import { EOL } from 'node:os';
+
+import { blue, green, red, yellow } from 'picocolors';
+import type { Formatter } from 'picocolors/types';
 
 /**
  * The audit configuration.
@@ -10,10 +14,8 @@ export type AuditOptions = {
   path: string;
 };
 
-type PackageJSON = {
-  dependencies: Record<string, string>;
-  devDependencies: Record<string, string>;
-};
+const VERSION_REGEX = /\d+\.\d+\.\d+/;
+// const CHARS = '┌─┬┐│├┼┤└┴┘•';
 
 /**
  * Checks dependency versions against the latest npm registry releases.
@@ -27,29 +29,102 @@ export async function audit({ path }: AuditOptions): Promise<void> {
 
   const { dependencies, devDependencies } = JSON.parse(
     readFileSync(path, 'utf-8')
-  ) as PackageJSON;
+  ) as Record<string, Record<string, string>>;
 
-  console.log('audit');
+  const queue: Record<string, Record<string, string>> = {
+    dependencies,
+    devDependencies
+  };
 
-  let keys = Object.keys(dependencies);
-  console.log('dependencies:', keys.length);
+  const queueKeys = Object.keys(queue);
 
-  for (const key of keys) {
-    const response: any = await fetch(`https://registry.npmjs.org/${key}`);
-    const json: any = await response.json();
-    const latest: string = json['dist-tags'].latest;
+  const lines: string[][] = [['PACKAGE', 'CURRENT', 'LATEST']];
 
-    console.log(`${key}:`, dependencies[key], '==>', latest);
+  while (queueKeys.length > 0) {
+    const name = queueKeys.shift()!;
+    const item = queue[name];
+    const keys = Object.keys(item);
+
+    for (const key of keys) {
+      const current = clean(item[key]);
+      const response = await fetch(`https://registry.npmjs.org/${key}`);
+
+      if (response.status !== 200) {
+        lines.push([key, current, '?.?.?']);
+        continue;
+      }
+
+      const json: any = await response.json();
+      const latest: string = json['dist-tags'].latest;
+
+      lines.push([key, current, latest]);
+    }
   }
 
-  keys = Object.keys(devDependencies);
-  console.log('devDependencies:', keys.length);
+  console.log(format(lines));
+}
 
-  for (const key of keys) {
-    const response = await fetch(`https://registry.npmjs.org/${key}`);
-    const json: any = await response.json();
-    const latest: string = json['dist-tags'].latest;
+function clean(version: string): string {
+  const match = VERSION_REGEX.exec(version);
+  return match ? match[0] : '?.?.?';
+}
 
-    console.log(`${key}:`, devDependencies[key], '==>', latest);
+function format(lines: string[][]): string {
+  const maxs = lines.reduce(
+    (p, c) => p.map((v, i) => Math.max(v, c[i].length)),
+    Array.from({ length: lines[0].length }, () => 0)
+  );
+
+  const table: string[] = [];
+  let str = '';
+
+  for (let i = 0; i < lines.length; ++i) {
+    let [key, current, latest] = lines[i];
+    const color = compare(current, latest);
+
+    key = key.padEnd(maxs[0]);
+    current = current.padEnd(maxs[1]);
+    latest = latest.padEnd(maxs[2]);
+
+    if (i) {
+      current = color(current);
+      latest = color(latest);
+    }
+
+    str = [key, current, latest].map(v => ` ${v} `).join('│');
+
+    table.push(`│${str}│`);
+
+    str = ['', '', '']
+      .map((v, j) => v.padEnd(maxs[j], '─'))
+      .map(v => `─${v}─`)
+      .join('┼');
+
+    str = `├${str}┤`;
+
+    if (i === lines.length - 1) {
+      str = str.replace('├', '└').replace('┤', '┘').replaceAll('┼', '┴');
+    }
+
+    table.push(str);
   }
+
+  table.unshift(str.replace('└', '┌').replace('┘', '┐').replaceAll('┴', '┬'));
+
+  return table.join(EOL);
+}
+
+function compare(current: string, latest: string): Formatter {
+  if (current === latest) {
+    return green;
+  }
+
+  const [a, b, c] = current.split('.').map(v => Number.parseInt(v));
+  const [x, y, z] = latest.split('.').map(v => Number.parseInt(v));
+
+  if (a < x) return red;
+  else if (b < y) return yellow;
+  else if (c < z) return blue;
+
+  return red;
 }
--- tests/audit.test.ts
+++ tests/audit.test.ts
@@ -6,10 +6,11 @@ import { describe, expect, it } from 'vitest';
 import { audit } from '../src/audit';
 
 describe('Test audit function', () => {
-  it('', async () => {
+  it('should audit the package.json file at the provided path', async () => {
     const path = join(process.cwd(), 'package.json');
 
     await audit({ path });
+    await audit({ path: join('wrong.json') });
 
     expect(existsSync(path)).toBe(true);
   });
